ab59187a3d7daf29effe9afe19593227f4dc75a3
caey11 <akuica64@gmail.com>
2025-08-04 20:40:28 -0700

    Add files via upload

---
+++ Bab4/src/bab4/Bab4.java
@@ -0,0 +1,89 @@
+//package Bab4;
+//
+//public class bab4 {
+//
+//    public static String variabel1;
+//    public static String variable2;
+//
+//    public static void main(String[] args) {
+//        System.out.println("variabel1 -> " + variabel1);
+//        System.out.println("variable2 -> " + variable2);
+//    }
+//
+//}
+
+//package Bab4;
+//
+//public class bab4 {
+//
+//    public static int var1, var2, var3;
+//
+//    public static void main(String[] args) {
+//        System.out.println("multi deklarasi -> " + " " + var1);
+//        System.out.println("multi deklarasi -> " + " " + var2);
+//        System.out.println("multi deklarasi -> " + " " + var3);
+//    }
+//
+//}
+
+//package Bab4;
+//
+//public class bab4 {
+//
+//    public static String variabel1;
+//    public static String variable2;
+//
+//    public static void main(String[] args) {
+//        System.out.println("variabel1 -> " + variabel1);
+//        System.out.println("variable2 -> " + variable2);
+//        System.out.println("setelah diisi :");
+//
+//        variabel1 = "Hello";
+//        variable2 = "Word";
+//
+//        System.out.println("variabel1 -> " + variabel1);
+//        System.out.println("variable2 -> " + variable2);
+//    }
+//
+//}
+
+//public class bab4 {
+//
+//    public static void main(String[] args) {
+//        String var1 = "1234567899";
+//        String var2 = "3.14";
+//        String var3 = "1";
+//
+//        int bulat = Integer.parseInt(var1);
+//        double pecahan = Double.parseDouble(var2);
+//        short bulat2 = Short.parseShort(var3);
+//
+//        System.out.println("variabel var1 = " + var1);
+//        System.out.println("variabel var2 = " + var2);
+//        System.out.println("variabel var3 = " + var3);
+//        System.out.println("----");
+//        System.out.println("konversi bilangan bulat = " + bulat);
+//        System.out.println("konversi bilangan pecahan = " + pecahan);
+//        System.out.println("konversi bilangan bulat2 = " + bulat2);
+//    }
+//}
+
+//public class bab4 {
+//
+//    public static void main(String[] args) {
+//        int var1 = 5;
+//        int var2 = 3;
+//
+//        System.out.println("Hasil : " + var1 / var2);
+//    }
+//}
+//
+//public class bab4 {
+//
+//    public static void main(String[] args) {
+//        int var1 = 5;
+//        int var2 = 3;
+//
+//        System.out.println("Hasil : " + (double)var1 / var2);
+//    }
+//}
